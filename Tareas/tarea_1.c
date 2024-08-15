04302cdaff49af9caeadd8b2d0ce9d3150a5fb7e
Fernando Choque <42763729+ferok123@users.noreply.github.com>
2024-08-15 17:40:31 -0300

    Create tarea_1.c
    Suma de dos números

---
+++ Tareas/tarea_1.c
@@ -0,0 +1,13 @@
+//suma  de Dos numeros 
+#include <stdio.h>
+int main()
+{
+  int a,b;                        //variable dos enteros
+  printf("Ingrese dos numeros");  //imprimo por pantalla
+  scanf("%d %d , &a, &b);
+  printf("\n Suma: %d , a+b);
+
+    return 0;
+  
+}
+
